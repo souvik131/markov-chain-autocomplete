5667872af64d26f0bc49d26c1dcb19d021632189
Souvik <souvik131@gmail.com>
2020-07-20 15:16:06 +0530

    few edits in run.py

--- run.py
+++ run.py
@@ -1,5 +1,30 @@
 
 import sys
+from recommendation import RecommendationEngine
+import json
+
+
+def getModal():
+    data=[]
+    modal={}
+    with open('data/queryCleanData.json', 'r') as fp:
+        queryData= json.load(fp)
+    keysQuery=[]
+    keysQuery.extend(list(queryData.keys()))
+    print(keysQuery)
+    for projectId in keysQuery:
+        try:
+            with open('data/'+projectId+'.p', 'rb') as fp:
+                reco = RecommendationEngine(5,7)
+                reco.fit(projectId)
+                modal[projectId]=reco
+        except Exception as e:
+            print(e)
+    return modal
+
+
+modal=getModal()         
+            
 
 def query_recommendation(projectId,query):
 	if projectId in modal:
